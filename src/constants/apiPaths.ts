82d6e62ace8b5ed0097f8e06013909be6f0db577
Dzianis_Shutikov <d.shutikov@gmail.com>
2020-11-15 21:59:47 +0300

    feat: Made integration with import service

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@
 const API_PATHS = {
   product: 'https://a31plw1kl5.execute-api.eu-west-1.amazonaws.com/dev/product',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://7wwynd3sf6.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev'
 };
 
